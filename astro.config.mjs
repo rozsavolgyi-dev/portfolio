8c04de238f91a1977919a60fefc4a3b9f744cb91
Krenedits Ferenc <fefe.kre@gmail.com>
2026-02-23 21:01:13 +0100

    typography fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   i18n: {
     defaultLocale: "hu",
     locales: ["hu", "en"],
